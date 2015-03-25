dec8dc46156c003b256e18c6442d467cfead8512
Jess Champion <jess.champion@solnet.co.nz>
2015-03-26 10:19:06 +1300

    forever broken...

--- src/ios/SnitchPlugin.m
+++ src/ios/SnitchPlugin.m
@@ -5,13 +5,8 @@
 #import <HockeySDK/HockeySDK.h>
 
 #import "SnitchPlugin.h"
-#import "DDFileLogger.h"
-
-extern int ddLogLevel;
 
 @interface SnitchPlugin ()
-
-@property (nonatomic) DDFileLogger *fileLogger;
 
 @end
 
@@ -20,29 +15,12 @@ extern int ddLogLevel;
 #pragma mark Initialization
 
 - (void)pluginInitialize {
-   // NSString * hockeyAppKey = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"HockeyApp App ID"];
-   // if( hockeyAppKey!=nil && [hockeyAppKey isEqualToString:@""]==NO && [hockeyAppKey rangeOfString:@"HOCKEY_APP_KEY"].location == NSNotFound ){
 
-        // initialize before HockeySDK, so the delegate can access the file logger
-        self.fileLogger = [[DDFileLogger alloc] init];
-        self.fileLogger.maximumFileSize = (1024 * 64); // 64 KByte
-        self.fileLogger.logFileManager.maximumNumberOfLogFiles = 1;
- //       [self.fileLogger rollLogFileWithCompletionBlock:nil];
-        [DDLog addLogger:self.fileLogger];
-        
-     //   [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:hockeyAppKey liveIdentifier:hockeyAppKey delegate:self];
-        
-     //   [[BITHockeyManager sharedHockeyManager] startManager];
-     //   [[BITHockeyManager sharedHockeyManager].authenticator authenticateInstallation];
-    //}
-    
-    DDLogInfo(@"HockeyApp Plugin initialized");
 }
 
 #pragma mark Plugin methods
 
 - (void)forcecrash:(CDVInvokedUrlCommand *)command {
-  DDLogWarn(@"HockeyApp Plugin crashing the app!");
   __builtin_trap();
 }
 
@@ -51,27 +29,6 @@ extern int ddLogLevel;
 // get the log content with a maximum byte size
 - (NSString *) getLogFilesContentWithMaxSize:(NSInteger)maxSize {
     NSMutableString *description = [NSMutableString string];
-    
-    NSArray *sortedLogFileInfos = [[_fileLogger logFileManager] sortedLogFileInfos];
-    NSInteger count = [sortedLogFileInfos count];
-    
-    // we start from the last one
-    for (NSInteger index = count - 1; index >= 0; index--) {
-        DDLogFileInfo *logFileInfo = [sortedLogFileInfos objectAtIndex:index];
-        
-        NSData *logData = [[NSFileManager defaultManager] contentsAtPath:[logFileInfo filePath]];
-        if ([logData length] > 0) {
-            NSString *result = [[NSString alloc] initWithBytes:[logData bytes]
-                                                        length:[logData length]
-                                                      encoding: NSUTF8StringEncoding];
-            
-            [description appendString:result];
-        }
-    }
-    
-    if ([description length] > maxSize) {
-        description = (NSMutableString *)[description substringWithRange:NSMakeRange([description length]-maxSize-1, maxSize)];
-    }
     
     return description;
 }
