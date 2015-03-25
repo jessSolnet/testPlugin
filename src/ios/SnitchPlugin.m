c2fa5f83ccd5b8096be73c032132a329b093e630
Jess Champion <jess.champion@solnet.co.nz>
2015-03-26 12:59:10 +1300

    soo so so so close

--- src/ios/SnitchPlugin.m
+++ src/ios/SnitchPlugin.m
@@ -24,46 +24,46 @@
 // Called to handle a pending crash report.
 //
 - (void) handleCrashReport {
-//    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
-//    NSData *crashData;
-//    NSError *error;
-//
-//     // Try loading the crash report
-//     crashData = [crashReporter loadPendingCrashReportDataAndReturnError: &error];
-//     if (crashData == nil) {
-//         NSLog(@"Could not load crash report: %@", error);
-//         // Purge the report
-//         [crashReporter purgePendingCrashReport];
-//         return;
-//     }
-//
-//    // We could send the report from here, but we'll just print out
-//    // some debugging info instead
-//    PLCrashReport *report = [[PLCrashReport alloc] initWithData: crashData error: &error];
-//    if (report == nil) {
-//        NSLog(@"Could not parse crash report");
-//        // Purge the report
-//        [crashReporter purgePendingCrashReport];
-//        return;
-//    }
-//
-//    NSLog(@"Crashed on %@", report.systemInfo.timestamp);
-//    NSLog(@"Crashed with signal %@ (code %@, address=0x%" PRIx64 ")", report.signalInfo.name,
-//    report.signalInfo.code, report.signalInfo.address);
+    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
+    NSData *crashData;
+    NSError *error;
+
+     // Try loading the crash report
+     crashData = [crashReporter loadPendingCrashReportDataAndReturnError: &error];
+     if (crashData == nil) {
+         NSLog(@"Could not load crash report: %@", error);
+         // Purge the report
+         [crashReporter purgePendingCrashReport];
+         return;
+     }
+
+    // We could send the report from here, but we'll just print out
+    // some debugging info instead
+    PLCrashReport *report = [[PLCrashReport alloc] initWithData: crashData error: &error];
+    if (report == nil) {
+        NSLog(@"Could not parse crash report");
+        // Purge the report
+        [crashReporter purgePendingCrashReport];
+        return;
+    }
+
+    NSLog(@"Crashed on %@", report.systemInfo.timestamp);
+    NSLog(@"Crashed with signal %@ (code %@, address=0x%" PRIx64 ")", report.signalInfo.name,
+    report.signalInfo.code, report.signalInfo.address);
     NSLog(@"Crashed");
 
 }
 
 // from UIApplicationDelegate protocol
 - (void) applicationDidFinishLaunching: (UIApplication *) application {
-//    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
-//    NSError *error;
-//    // Check if we previously crashed
-//    if ([crashReporter hasPendingCrashReport])
-//        [self handleCrashReport];
-//    // Enable the Crash Reporter
-//    if (![crashReporter enableCrashReporterAndReturnError: &error])
-//       NSLog(@"Warning: Could not enable crash reporter: %@", error);
+    PLCrashReporter *crashReporter = [PLCrashReporter sharedReporter];
+    NSError *error;
+    // Check if we previously crashed
+    if ([crashReporter hasPendingCrashReport])
+        [self handleCrashReport];
+    // Enable the Crash Reporter
+    if (![crashReporter enableCrashReporterAndReturnError: &error])
+       NSLog(@"Warning: Could not enable crash reporter: %@", error);
     NSLog(@"Launched");
 
 }
