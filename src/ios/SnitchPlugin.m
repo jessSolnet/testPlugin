21fe7e5713a8c012ded842636d902797fe344edd
Jess Champion <jess.champion@solnet.co.nz>
2015-07-28 10:55:44 +1200

    changed url to wifi url

--- src/ios/SnitchPlugin.m
+++ src/ios/SnitchPlugin.m
@@ -26,7 +26,7 @@
     
     NSString * encodedMessage = (NSString *)  CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(NULL, (CFStringRef) message, NULL, (CFStringRef) @"!*'();:@&=+$,/?%#[]", kCFStringEncodingUTF8));
     
-    NSString *urlString = [NSString stringWithFormat: @"%@%@", @"http://jess-laptop.local:8080/snitchspring/CrashListener?data=", encodedMessage];
+    NSString *urlString = [NSString stringWithFormat: @"%@%@", @"http://10.1.140.148:8080/snitchspring/CrashListener?data=", encodedMessage];
     
     NSURL *url = [NSURL URLWithString:urlString];
     
